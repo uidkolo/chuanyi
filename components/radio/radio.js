ee810e6de6027ef147f3f5a91a7e95a74a30aa4e
huyang <uidkolo@163.com>
2018-11-19 19:23:03 +0800

    add project

---
+++ components/radio/radio.js
@@ -0,0 +1,28 @@
+// components/radio.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    value: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    changeValue(){
+
+    }
+  }
+})
